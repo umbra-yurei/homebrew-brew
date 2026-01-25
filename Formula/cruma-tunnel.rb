ec0ba45de637afa618c09ac7b16e47727a968a46
Olof <olof@twnet.se>
2026-01-25 13:26:42 +0100

    0.3.0-beta.2

--- Formula/cruma-tunnel.rb
+++ Formula/cruma-tunnel.rb
@@ -1,10 +1,10 @@
 class CrumaTunnel < Formula
   desc "cruma-tunnel CLI"
   homepage "https://cruma.io"
-  version "0.3.0-beta.1"
+  version "0.3.0-beta.2"
 
-  url "https://files.cruma.io/files/tunnel-agent%2Fv0.3.0-beta.1%2Faarch64-apple-darwin%2Fcruma-tunnel"
-  sha256 "0f05dacc474df183d0f277a7351d8390158867ca74b1bc28c8f1e479d15ac380"
+  url "https://files.cruma.io/files/tunnel-agent%2Fv0.3.0-beta.2%2Faarch64-apple-darwin%2Fcruma-tunnel"
+  sha256 "1ba9269ba2ebea37f752a97e5be918ffecd84c8a551aa87c13fa100110f8403a"
 
   def install
     bin.install "cruma-tunnel"
